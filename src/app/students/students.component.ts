3e7d5e80fb1917878ed53bcc50e13161fd0ac5e1
r20031646 <ramdani.meriem2001@gmail.com>
2023-01-30 15:00:26 +0100

    initial commit

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+
+import { Component,OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
   styleUrls: ['./students.component.css']
 })
-export class StudentsComponent {
-
+export class StudentsComponent implements OnInit {
+  studentName: string = 'Henri';
+  studentStatus : string = 'present';
+  constructor() { }
+  ngOnInit() {
+  }
+  getStatus() {
+    return this.studentStatus; }
 }
